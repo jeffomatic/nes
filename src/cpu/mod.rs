9b9ba43286a52b0d27e2229e70c9a0d633e9b09a
Jeff Lee <jeffomatic@gmail.com>
2020-01-18 11:52:55 -0800

    cpu: begin fetch_operand() implementation

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -1,5 +1,8 @@
 use self::address_mode::AddressMode;
 use self::opcode::Opcode;
+use self::registers::Registers;
+use super::math;
+use super::memory::Memory;
 
 mod address_mode;
 mod opcode;
@@ -163,6 +166,79 @@ fn decode(raw_opcode: u8) -> Option<(Opcode, AddressMode)> {
         0xFE => Some((Opcode::Inc, AddressMode::AbsoluteX)),
         _ => None,
     }
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+enum Opval {
+    Byte(u8),
+    Address(u16),
+}
+
+fn fetch_operand(regs: &Registers, mem: &Memory, addr_mode: AddressMode) -> Option<Opval> {
+    match addr_mode {
+        AddressMode::Implicit => None,
+        AddressMode::Accumulator => Some(Opval::Byte(regs.a)),
+        AddressMode::Immediate => Some(Opval::Byte(mem.read(regs.pc + 1))),
+        AddressMode::ZeroPage => Some(Opval::Byte(mem.read(mem.read(regs.pc + 1) as u16))),
+        AddressMode::ZeroPageX => Some(Opval::Byte(
+            mem.read(mem.read(regs.pc + 1) as u16 + regs.x as u16),
+        )),
+        AddressMode::ZeroPageY => Some(Opval::Byte(
+            mem.read(mem.read(regs.pc + 1) as u16 + regs.y as u16),
+        )),
+        AddressMode::Relative => Some(Opval::Address(math::byte_addr_offset(
+            regs.pc,
+            mem.read(regs.pc + 1),
+        ))),
+        _ => unimplemented!(),
+    }
+}
+
+#[test]
+fn test_fetch_operand() {
+    let mut regs = Registers::default();
+    regs.a = 0x01;
+    regs.x = 0x02;
+    regs.y = 0x03;
+    regs.pc = 0x200;
+
+    let mut mem = Memory::new();
+    mem.write(0xEE, 0xAA);
+    mem.write(0xF0, 0xBB);
+    mem.write(0xF1, 0xCC);
+    mem.write(0x201, 0xEE);
+
+    assert_eq!(fetch_operand(&regs, &mem, AddressMode::Implicit), None);
+
+    assert_eq!(
+        fetch_operand(&regs, &mem, AddressMode::Accumulator),
+        Some(Opval::Byte(0x01))
+    );
+
+    assert_eq!(
+        fetch_operand(&regs, &mem, AddressMode::Immediate),
+        Some(Opval::Byte(0xEE))
+    );
+
+    assert_eq!(
+        fetch_operand(&regs, &mem, AddressMode::ZeroPage),
+        Some(Opval::Byte(0xAA))
+    );
+
+    assert_eq!(
+        fetch_operand(&regs, &mem, AddressMode::ZeroPageX),
+        Some(Opval::Byte(0xBB))
+    );
+
+    assert_eq!(
+        fetch_operand(&regs, &mem, AddressMode::ZeroPageY),
+        Some(Opval::Byte(0xCC))
+    );
+
+    assert_eq!(
+        fetch_operand(&regs, &mem, AddressMode::Relative),
+        Some(Opval::Address(0x1EE))
+    );
 }
 
 fn run(state: &mut state::State) {
