653fa40e3737a611640938f040f862b71175e98f
Jeff Lee <jeffomatic@gmail.com>
2020-01-24 22:47:23 -0800

    remove instruction::decode, add instruction cycle accounting

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -6,8 +6,11 @@ mod state;
 mod status;
 
 fn next(cpu: &mut state::Cpu) {
-    let (opcode_type, operand, _cycles) = instruction::decode(cpu).unwrap();
+    let (opcode_type, addr_mode, base_cost) =
+        opcode::decode(cpu.consume_instruction_byte()).unwrap();
+    let (operand, operand_cost) = operand::decode(cpu, opcode_type, addr_mode);
     instruction::execute(opcode_type, cpu, operand);
+    cpu.cycle_add(base_cost + operand_cost);
 }
 
 #[test]
@@ -24,6 +27,7 @@ fn test() {
     assert_eq!(cpu.regs.pc, 2);
     assert_eq!(cpu.regs.a, 1);
     assert_eq!(cpu.regs.p, 0);
+    assert_eq!(cpu.cycles, 2);
 
     next(&mut cpu);
     assert_eq!(cpu.regs.pc, 4);
@@ -32,6 +36,7 @@ fn test() {
         cpu.regs.p,
         status::Status::Carry.mask() | status::Status::Zero.mask()
     );
+    assert_eq!(cpu.cycles, 4);
 
     next(&mut cpu);
     assert_eq!(cpu.regs.pc, 6);
@@ -40,4 +45,5 @@ fn test() {
         cpu.regs.p,
         status::Status::Carry.mask() | status::Status::Zero.mask()
     );
+    assert_eq!(cpu.cycles, 6);
 }
