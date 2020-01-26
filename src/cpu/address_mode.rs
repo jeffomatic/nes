1104dc4e23b3a6698299c069ce33cda452129f6e
Jeff Lee <jeffomatic@gmail.com>
2020-01-26 13:52:01 -0800

    add AddressMode#operand_size()

--- src/cpu/address_mode.rs
+++ src/cpu/address_mode.rs
@@ -15,3 +15,23 @@ pub enum AddressMode {
     IndirectX, // aka "indexed indirect"
     IndirectY, // aka "indirect indexed"
 }
+
+impl AddressMode {
+    pub fn operand_size(self) -> usize {
+        match self {
+            Self::Implicit => 0,
+            Self::Accumulator => 0,
+            Self::Immediate => 1,
+            Self::ZeroPage => 1,
+            Self::ZeroPageX => 1,
+            Self::ZeroPageY => 1,
+            Self::Relative => 1,
+            Self::Absolute => 2,
+            Self::AbsoluteX => 2,
+            Self::AbsoluteY => 2,
+            Self::Indirect => 2,
+            Self::IndirectX => 1,
+            Self::IndirectY => 1,
+        }
+    }
+}
